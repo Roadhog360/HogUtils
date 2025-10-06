cd72b2eefa35ffc4d38579ca681e633386b98af0
roadhog360 <11903511+Roadhog360@users.noreply.github.com>
2025-10-06 15:21:09 -0500

    De-extend OreRegisterEventPre.java from OreRegisterEvent Fixes #2
    Signed-off-by: roadhog360 <11903511+Roadhog360@users.noreply.github.com>

--- src/main/java/roadhog360/hogutils/api/event/OreRegisterEventPre.java
+++ src/main/java/roadhog360/hogutils/api/event/OreRegisterEventPre.java
@@ -1,15 +1,20 @@
 package roadhog360.hogutils.api.event;
 
 import cpw.mods.fml.common.eventhandler.Cancelable;
+import cpw.mods.fml.common.eventhandler.Event;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.oredict.OreDictionary;
 
 /// {@link Cancelable} version of the Ore Dictionary register event; for the purpose of preventing OreDictionary tags from being registered.
 /// Will also cancel auto-tagging logic for this OreDictionary tag.
 /// Do not register new OreDictionary values here, you are very likely to trigger infinite recursion!
-public class OreRegisterEventPre extends OreDictionary.OreRegisterEvent {
-    public OreRegisterEventPre(String name, ItemStack ore) {
-        super(name, ore);
+public class OreRegisterEventPre extends Event {
+    public final String name;
+    public final ItemStack stack;
+
+    public OreRegisterEventPre(String name, ItemStack ore)
+    {
+        this.name = name;
+        this.stack = ore;
     }
 
     @Override
